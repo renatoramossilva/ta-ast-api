982a683882a19a51f5355b0e05b5285f82f7c851
Renato Silva <renato.silva@ctw.bmwgroup.com>
2024-12-08 18:49:04 +0000

    Move `get_db` function to `crud.py`

--- backend/app/database/crud.py
+++ backend/app/database/crud.py
@@ -3,6 +3,14 @@ from . import models
 from .database import SessionLocal
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        return db
+    finally:
+        db.close()
+
+
 def create_hotel(name: str, address: str, description: str, review: float, db: Session):
     db_hotel = models.Hotel(name=name, address=address, description=description, review=review)
     print(db_hotel)
