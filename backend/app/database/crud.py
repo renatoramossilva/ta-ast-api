aee6a92f8cbcff329866c051cd4593a2b0dc592c
Renato Silva <renato.silva@ctw.bmwgroup.com>
2024-12-08 18:43:30 +0000

    Create function to save on database
    This commit adds a new function to save data to the database.
    
    Changes include:
    - Added `crud.py` to handle database operations.

---
+++ backend/app/database/crud.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Session
+from . import models
+from .database import SessionLocal
+
+
+def create_hotel(name: str, address: str, description: str, review: float, db: Session):
+    db_hotel = models.Hotel(name=name, address=address, description=description, review=review)
+    print(db_hotel)
+    print(db)
+    db.add(db_hotel)
+    db.commit()
+    db.refresh(db_hotel)
+    return db_hotel
