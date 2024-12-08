3497b4a06be8eba8f3dd933f5aef6a28524a55ba
Renato Silva <renato.silva@ctw.bmwgroup.com>
2024-12-08 18:47:50 +0000

    Refactor database files

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,12 +1,10 @@
-# database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/postgres")
+DATABASE_URL = "postgresql://postgres:postgres@db:5432/postgres"
 
-engine = create_engine(DATABASE_URL, connect_args={})
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
