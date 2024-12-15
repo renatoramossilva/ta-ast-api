6ca7628772e7044b50b0e7fb37299832aebd6017
Renato Silva <renato.silva@ctw.bmwgroup.com>
2024-12-15 21:37:02 +0000

    WIP: Add Amenity entity to the database

--- backend/app/database/models.py
+++ backend/app/database/models.py
@@ -2,8 +2,9 @@
 
 from typing import Any
 
-from sqlalchemy import Column, Float, Integer, String
+from sqlalchemy import Column, Float, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base: Any = declarative_base()
 
@@ -19,8 +20,25 @@ class Hotel(Base):  # pylint: disable=too-few-public-methods
     description = Column(String)
     review = Column(Float)
 
+    amenities = relationship(
+        "Amenity", back_populates="hotel", cascade="all, delete-orphan"
+    )
+
     def __repr__(self):
         return (
             f"<Hotel(name={self.name}, address={self.address}, "
             f"description={self.description}, review={self.review})>"
         )
+
+
+class Amenity(Base):
+    __tablename__ = "amenities"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    hotel_id = Column(Integer, ForeignKey("hotels.id"), nullable=False)
+
+    hotel = relationship("Hotel", back_populates="amenities")
+
+    def __repr__(self):
+        return f"<Amenity(name={self.name})>"
